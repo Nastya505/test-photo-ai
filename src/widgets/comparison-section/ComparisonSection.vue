55e5f90c51d0c06863f17f985f4379263852bade
“Anastasia” <555.555.555.anastasie@gmail.com>
2025-05-01 12:29:45 +0700

    fix: add type for comparisons

--- src/widgets/comparison-section/ComparisonSection.vue
+++ src/widgets/comparison-section/ComparisonSection.vue
@@ -8,48 +8,9 @@ import { Swiper, SwiperSlide } from 'swiper/vue';
 import { ref } from 'vue';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { comparisons } from '@/shared/data/comparisons';
 
 const isDesktop = useMediaQuery('(min-width: 1023px)');
-
-const comparisons = [
-  {
-    name: 'Midjourney 3',
-    image: '/images/mj3.webp',
-    features: [
-      { label: 'Resemblance', value: 'Low resemblance', enabled: false },
-      { label: 'Character', value: 'Inconsistent character', enabled: false },
-      { label: 'Photorealism', value: 'Medium photorealism', enabled: true },
-      { label: 'Resolution', value: 'Low resolution', enabled: false },
-      { label: 'Ethnicity', value: 'Maintains ethnicity', enabled: true },
-      { label: 'Clarity', value: 'Clear and sharp', enabled: true },
-    ],
-  },
-  {
-    name: 'Artphoto.AI',
-    image: '/images/artphoto.webp',
-    highlight: true,
-    features: [
-      { label: 'Resemblance', value: 'High resemblance', enabled: true },
-      { label: 'Character', value: 'Consistent character', enabled: true },
-      { label: 'Photorealism', value: 'High photorealism', enabled: true },
-      { label: 'Resolution', value: 'High resolution', enabled: true },
-      { label: 'Ethnicity', value: 'Maintains ethnicity', enabled: true },
-      { label: 'Clarity', value: 'Clear and sharp', enabled: true },
-    ],
-  },
-  {
-    name: 'DALL-E 3',
-    image: '/images/dalle3.webp',
-    features: [
-      { label: 'Resemblance', value: 'No resemblance' },
-      { label: 'Character', value: 'Inconsistent character', enabled: false },
-      { label: 'Photorealism', value: 'Low photorealism', enabled: false },
-      { label: 'Resolution', value: 'No resolution', enabled: false },
-      { label: 'Ethnicity', value: 'Maintains ethnicity', enabled: true },
-      { label: 'Clarity', value: 'Not clear and not sharp', enabled: false },
-    ],
-  },
-];
 
 const swiperRef = ref<SwiperType | null>(null);
 watch(isDesktop, (value) => {
